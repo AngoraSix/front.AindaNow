aed9574def5740fbfd2fb45fe5378568ff04f2e1
Ger Roza <rozagerardo@gmail.com>
2021-12-26 13:41:54 -0300

    Added New Project button, and modified projects toolbar layout

--- components/ProjectsList/ProjectsList.component.js
+++ components/ProjectsList/ProjectsList.component.js
@@ -1,7 +1,9 @@
-import { Button, TextField } from '@mui/material';
+import { Button, IconButton, TextField } from '@mui/material';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import ProjectCard from '../ProjectCard';
+import NewIcon from '@mui/icons-material/AddCircleOutline';
+import NewIconContained from '@mui/icons-material/AddCircle';
 
 const ProjectsList = ({ total, projectsList, onNextPageClick, onSearch }) => {
   const [search, setSearch] = useState('');
@@ -15,18 +17,30 @@ const ProjectsList = ({ total, projectsList, onNextPageClick, onSearch }) => {
   return (
     <div className="ProjectsList ProjectsList__Container">
       <div className="ProjectsList__Toolbar">
-        <div className="ProjectsList__Toolbar__Column">
-          <div>
-            Mostrando {projectsList.length} de {total}
-          </div>
-        </div>
-        <div className="ProjectsList__Toolbar__Column" />
-        <div className="ProjectsList__Toolbar__Column">
+        <div className="ProjectsList__Toolbar__Column Column__Large">
           <TextField
-            label="Buscar"
+            label="Search"
             value={search}
             onChange={onSearchChange}
+            fullWidth
           />
+        </div>
+        <div className="ProjectsList__Toolbar__Column">
+          <Button
+            color="primary"
+            variant="contained"
+            startIcon={<NewIcon />}
+            sx={{ display: { xs: 'none', sm: 'flex' } }}
+          >
+            Create
+          </Button>
+          <IconButton
+            aria-label="create"
+            color="primary"
+            sx={{ display: { xs: 'flex', sm: 'none' } }}
+          >
+            <NewIconContained />
+          </IconButton>
         </div>
       </div>
 
