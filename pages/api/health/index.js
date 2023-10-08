0e69e4df0666e9a7098f4e677138a768973b4d6f
Ger Roza <rozagerardo@gmail.com>
2023-10-08 11:34:48 -0300

    Adapting for CI plan building and pushing docker image - 9 - added console checks

--- pages/api/health/index.js
+++ pages/api/health/index.js
@@ -1,5 +1,8 @@
 const page = async (req, res) => {
+  console.log("CHECKING HEALTH!");
+  console.log(req.method);
   if (req.method === 'GET') {
+    console.log("CHecking Health with GET");
     const healthData = {
       status: 'RUNNING',
     };
