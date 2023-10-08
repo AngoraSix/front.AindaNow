31dd8ac66ed2437b5ce0686e56fcd8cb01732d51
Ger Roza <rozagerardo@gmail.com>
2023-10-07 21:49:24 -0300

    Adapting for CI plan building and pushing docker image

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,9 @@ WORKDIR /app
 
 # Install dependencies based on the preferred package manager
 COPY package.json package-lock.json ./
-RUN npm install --omit=dev
+COPY next.config.js ./
+RUN npm ci;
+# RUN npm install --omit=dev
 
 
 # Rebuild the source code only when needed
--- pages/api/upload/file.js
+++ pages/api/upload/file.js
@@ -1,12 +1,12 @@
-import { getToken } from 'next-auth/jwt';
-import { oauthFrameworkConfig } from '../../../config/oauth';
-import MethodNotAllowedError from '../../../utils/errors/MethodNotAllowedError';
-import { getEnv } from '../../../utils/env';
-import api from '../../../api';
 import multer from 'multer';
+import { getToken } from 'next-auth/jwt';
 import nextConnect from 'next-connect';
+import api from '../../../api';
 import serviceConfig from '../../../config';
+import { oauthFrameworkConfig } from '../../../config/oauth';
+import { getEnv } from '../../../utils/env';
 import InternalServerError from '../../../utils/errors/InternalServerError';
+import MethodNotAllowedError from '../../../utils/errors/MethodNotAllowedError';
 import logger from '../../../utils/logger';
 
 const upload = multer();
@@ -51,17 +51,24 @@ apiRoute.post(async (req, res) => {
   const { headers, files, body } = req;
   const validatedToken =
     token?.error !== 'RefreshAccessTokenError' ? token : null;
-  try {
-    const response = await api.media.uploadImages(files, validatedToken);
-    res.status(200).json(response);
-  } catch (err) {
-    const errorMessage = 'Error uploading file',
-      internalServerErr = new InternalServerError(errorMessage, 'FILE_UPLOAD');
-    logger.error(
-      errorMessage,
-      typeof err === 'object' ? JSON.stringify(err) : err
-    );
-    res.status(internalServerErr.status).json(internalServerErr.asObject());
+  if (validatedToken != null) {
+    try {
+      const response = await api.media.uploadImages(files, validatedToken);
+      res.status(200).json(response);
+    } catch (err) {
+      const errorMessage = 'Error uploading file',
+        internalServerErr = new InternalServerError(
+          errorMessage,
+          'FILE_UPLOAD'
+        );
+      logger.error(
+        errorMessage,
+        typeof err === 'object' ? JSON.stringify(err) : err
+      );
+      res.status(internalServerErr.status).json(internalServerErr.asObject());
+    }
+  } else {
+    res.status(401);
   }
 });
 
