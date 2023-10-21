908d937d762f18bd79173353e4e0d934335d760b
Ger Roza <rozagerardo@gmail.com>
2023-10-21 15:17:13 -0300

    Fixing error in method signatures

--- utils/infra/index.js
+++ utils/infra/index.js
@@ -1,10 +1,10 @@
 import { obtainGoogleHeaders } from './googleInfra';
 
-export const obtainInfraHeaders = async (infraConfigs, baseUrl, headers) => {
-  console.log("GERGER 0 - INFRA");
+export const obtainInfraHeaders = async (infraConfigs, baseUrl) => {
+  console.log('GERGER 0 - INFRA');
   console.log(infraConfigs);
   if (infraConfigs.isGoogleCloudRun) {
-    return await obtainGoogleHeaders(baseUrl, headers);
+    return await obtainGoogleHeaders(baseUrl);
   }
   return {};
 };
