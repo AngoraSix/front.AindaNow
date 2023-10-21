ec753591025642bf0cc16b4efab0a37822c1ef7d
Ger Roza <rozagerardo@gmail.com>
2023-10-21 14:29:52 -0300

    Debugging Google Cloud Run - fix error in URL

--- utils/infra/googleInfra.js
+++ utils/infra/googleInfra.js
@@ -3,19 +3,26 @@ import { GoogleAuth } from 'google-auth-library';
 export const obtainGoogleHeaders = async (baseUrl) => {
   console.log("GERGER 1 - It's GCP");
   console.log(baseUrl);
-  console.log(baseUrl + "/");
+  console.log(baseUrl + '/');
   const auth = new GoogleAuth();
   const client = await auth.getIdTokenClient(baseUrl);
   console.log('GERGER 2');
   console.log(client.idTokenProvider?.constructor?.name);
   console.log('GERGER 2.2');
 
-  const res = await client.request({url: `${baseUrl} + "/projects/presentations"`});
-  console.log(res.data);
-  console.log('GERGER 2.3');
-  
+  try {
+    const res = await client.request({
+      url: `${baseUrl}/projects/presentations`,
+    });
+    console.log(res.data);
+    console.log('GERGER 2.3');
+  } catch (ex) {
+    console.log('GERGER 2.3 ERROR');
+    console.log(ex);
+  }
+
   const idToken = await client.idTokenProvider.fetchIdToken();
-  console.log("GERGER3");
+  console.log('GERGER3');
   console.log(idToken);
   return {
     'X-Serverless-Authorization': `Bearer ${idToken}`,
