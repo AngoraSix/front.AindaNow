b2ac2a881f12fa32335ed3f97bd60c3e44c40a10
Ger Roza <rozagerardo@gmail.com>
2023-10-21 15:14:35 -0300

    Trying new approach - no cache - manual fetch of IdToken

--- api/projects.js
+++ api/projects.js
@@ -10,45 +10,41 @@ class ProjectsAPI {
     const headers = this.axios.getCommonHeaders();
     const authHeaders = this.axios.getAuthorizationHeaders(token, false);
 
-    console.log('GERGERGER 99 - FETCHPROJECTS');
-    // console.log(this.axios.getBaseURL().);
-    console.log(config.api.serverBaseURL);
-    console.log(this.axios.serverBaseURL);
-    console.log('GERGERGER 99 - FETCHPROJECTS FINISH LOGS');
+    // console.log('GERGERGER 99 - FETCHPROJECTS');
+    // // console.log(this.axios.getBaseURL().);
+    // console.log(config.api.serverBaseURL);
+    // console.log(this.axios.serverBaseURL);
+    // console.log('GERGERGER 99 - FETCHPROJECTS FINISH LOGS');
 
     const infraHeaders = await obtainInfraHeaders(
       config.infra,
-      config.api.serverBaseURL,
-      {
-        ...headers,
-        ...authHeaders,
-      }
+      config.api.serverBaseURL
     );
 
-    console.log('GERGERGER 88 - A VERGER');
-    console.log(infraHeaders);
-    console.log(infraHeaders['X-Serverless-Authorization']);
+    // console.log('GERGERGER 88 - A VERGER');
+    // console.log(infraHeaders);
+    // console.log(infraHeaders['X-Serverless-Authorization']);
 
-    console.log('GERGERGER 77 - A VERGER');
-    console.log(
-      {
-        ...headers,
-        ...authHeaders,
-        ...infraHeaders,
-      }['X-Serverless-Authorization']
-    );
+    // console.log('GERGERGER 77 - A VERGER');
+    // console.log(
+    //   {
+    //     ...headers,
+    //     ...authHeaders,
+    //     ...infraHeaders,
+    //   }['X-Serverless-Authorization']
+    // );
 
-    console.log('WHATAMARATATA');
-    const data2 = await this.axios.get(
-      `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${config.api.serverBaseURL}`,
-      {
-        headers: {
-          'Metadata-Flavor': 'Google',
-        },
-      }
-    );
-    console.log(data2);
-    console.log(data2?.data);
+    // console.log('WHATAMARATATA');
+    // const data2 = await this.axios.get(
+    //   `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${config.api.serverBaseURL}`,
+    //   {
+    //     headers: {
+    //       'Metadata-Flavor': 'Google',
+    //     },
+    //   }
+    // );
+    // console.log(data2);
+    // console.log(data2?.data);
     const { data: projectPresentationdata } = await this.axios.get(
       `/presentations?${attributes}`,
       {
--- utils/infra/googleInfra.js
+++ utils/infra/googleInfra.js
@@ -1,31 +1,45 @@
-import { GoogleAuth } from 'google-auth-library';
+import axios from 'axios';
+import logger from '../logger';
 
-export const obtainGoogleHeaders = async (baseUrl, headers) => {
-  console.log("GERGER 1 - It's GCP");
-  console.log(baseUrl);
-  console.log(baseUrl + '/');
-  const auth = new GoogleAuth();
-  const client = await auth.getIdTokenClient(baseUrl);
-  console.log('GERGER 2');
-  console.log(client.idTokenProvider?.constructor?.name);
-  console.log('GERGER 2.2');
-
+export const obtainGoogleHeaders = async (baseUrl) => {
+  const headers = {};
   try {
-    const res = await client.request({
-      url: `${baseUrl}/projects/presentations`,
-      headers
-    });
-    console.log(res.data);
-    console.log('GERGER 2.3');
-  } catch (ex) {
-    console.log('GERGER 2.3 ERROR');
-    console.log(ex);
+    const { data: idToken } = await axios.get(
+      `http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=${baseUrl}`,
+      {
+        headers: {
+          'Metadata-Flavor': 'Google',
+        },
+      }
+    );
+    headers['X-Serverless-Authorization'] = idToken;
+  } catch (err) {
+    logger.error('Error obtaining GCP Id Token', err);
   }
 
-  const idToken = await client.idTokenProvider.fetchIdToken();
-  console.log('GERGER3');
-  console.log(idToken);
-  return {
-    'X-Serverless-Authorization': `Bearer ${idToken}`,
-  };
+  return headers;
+  // const auth = new GoogleAuth();
+  // const client = await auth.getIdTokenClient(baseUrl);
+  // console.log('GERGER 2');
+  // console.log(client.idTokenProvider?.constructor?.name);
+  // console.log('GERGER 2.2');
+
+  // try {
+  //   const res = await client.request({
+  //     url: `${baseUrl}/projects/presentations`,
+  //     headers
+  //   });
+  //   console.log(res.data);
+  //   console.log('GERGER 2.3');
+  // } catch (ex) {
+  //   console.log('GERGER 2.3 ERROR');
+  //   console.log(ex);
+  // }
+
+  // const idToken = await client.idTokenProvider.fetchIdToken();
+  // console.log('GERGER3');
+  // console.log(idToken);
+  // return {
+  //   'X-Serverless-Authorization': `Bearer ${idToken}`,
+  // };
 };
