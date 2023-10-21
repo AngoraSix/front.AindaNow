12029692dbb7b84fd12fc0ec2d3626deae8fd012
Ger Roza <rozagerardo@gmail.com>
2023-10-21 13:22:35 -0300

    Debugging Google Cloud Run - fixing google auth setup

--- utils/infra/googleInfra.js
+++ utils/infra/googleInfra.js
@@ -3,9 +3,12 @@ import { GoogleAuth } from 'google-auth-library';
 export const obtainGoogleHeaders = async (baseUrl) => {
   console.log("GERGER 1 - It's GCP");
   console.log(baseUrl);
+  console.log(baseUrl + "/");
   const auth = new GoogleAuth();
-  const client = await auth.getIdTokenClient(baseUrl);
+  const client = await auth.getIdTokenClient(baseUrl + "/");
   console.log('GERGER 2');
+  console.log(client.idTokenProvider);
+  console.log('GERGER 2.2');
   const idToken = await client.idTokenProvider?.fetchIdToken();
   console.log("GERGER3");
   console.log(idToken);
