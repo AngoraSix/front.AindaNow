551c40080d168c0c4d42a08d07e9248432836e5a
Ger Roza <rozagerardo@gmail.com>
2025-01-13 17:48:27 -0300

    Non related fixes

--- components/Project/ProjectPresentation/View/Sections/ProjectPresentation/Actions/ProjectPresentationActions.properties.js
+++ components/Project/ProjectPresentation/View/Sections/ProjectPresentation/Actions/ProjectPresentationActions.properties.js
@@ -1,10 +1,10 @@
+import config from "../../../../../../../config";
+
 export const GENERAL_CLUBS_ACTIONS_KEY = 'general-clubs-actions';
 
-const CONTRIBUTOR_CANDIDATES_TYPE_KEY = 'contributor-candidates';
-
 export const PROJECT_PRESENTATION_SUPPORTED_ACTIONS = {
-  SHOW_INTEREST: `${CONTRIBUTOR_CANDIDATES_TYPE_KEY}.addMember`,
-  WITHDRAW_INTEREST: `${CONTRIBUTOR_CANDIDATES_TYPE_KEY}.removeMember`,
+  SHOW_INTEREST: `${config.api.servicesAPIParams.clubsWellKnownContributorCandidatesType}.addMember`,
+  WITHDRAW_INTEREST: `${config.api.servicesAPIParams.clubsWellKnownContributorCandidatesType}.removeMember`,
   REGISTER_ALL_CLUBS: `${GENERAL_CLUBS_ACTIONS_KEY}.registerAll`,
   EDIT: 'updateProjectPresentation',
 };
